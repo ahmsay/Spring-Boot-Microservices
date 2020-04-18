21e9f0f08285d155960c8ab021c5abea522437b9
ahmsay <ahmsay@gmail.com>
2020-04-18 22:19:46 +0300

    organized api calls

--- movie-catalog-service/src/main/java/com/ravager/moviecatalogservice/controller/CatalogController.java
+++ movie-catalog-service/src/main/java/com/ravager/moviecatalogservice/controller/CatalogController.java
@@ -1,21 +1,42 @@
 package com.ravager.moviecatalogservice.controller;
 
 import com.ravager.moviecatalogservice.entity.Catalog;
+import com.ravager.moviecatalogservice.entity.Movie;
+import com.ravager.moviecatalogservice.entity.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
 
+    private final RestTemplate restTemplate;
+
+    public CatalogController(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @RequestMapping("/{userId}")
     public List<Catalog> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new Catalog("Titanic", "Test", 5)
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("1235", 4),
+                new Rating("0238", 1)
         );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class);
+            return new Catalog(movie.getName(), "test", rating.getRating());
+        })
+        .collect(Collectors.toList());
     }
 }
---
+++ movie-catalog-service/src/main/java/com/ravager/moviecatalogservice/entity/Rating.java
@@ -0,0 +1,28 @@
+package com.ravager.moviecatalogservice.entity;
+
+public class Rating {
+
+    private String movieId;
+    private int rating;
+
+    public Rating(final String movieId, final int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(final String movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(final int rating) {
+        this.rating = rating;
+    }
+}
