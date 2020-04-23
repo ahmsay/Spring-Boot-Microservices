9e8beb7ea99bf801c8b7fbd5d4782f7a665aab33
ahmsay <ahmsay@gmail.com>
2020-04-23 20:33:54 +0300

    hystrix parameters added

--- movie-catalog-service/src/main/java/com/ravager/moviecatalogservice/service/CatalogService.java
+++ movie-catalog-service/src/main/java/com/ravager/moviecatalogservice/service/CatalogService.java
@@ -1,6 +1,7 @@
 package com.ravager.moviecatalogservice.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.ravager.moviecatalogservice.entity.Catalog;
 import com.ravager.moviecatalogservice.entity.Movie;
 import com.ravager.moviecatalogservice.entity.Rating;
@@ -17,7 +18,12 @@ public class CatalogService implements ICatalogService {
     }
 
     @Override
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    })
     public Catalog getCatalog(final Rating rating) {
         Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         return new Catalog(movie.getName(), movie.getDescription(), rating.getRating());
