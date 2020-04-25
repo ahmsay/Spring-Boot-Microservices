4384ed0a66946c3d99620c675055f102f73b3468
ahmsay <ahmsay@gmail.com>
2020-04-25 10:55:00 +0300

    value annotation tricks

--- spring-boot-config/src/main/java/com/ravager/springbootconfig/controller/GreetingController.java
+++ spring-boot-config/src/main/java/com/ravager/springbootconfig/controller/GreetingController.java
@@ -4,14 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: getting greeting failed}")
     private String myGreeting;
+
+    @Value("${my.list.values}")
+    private List<String> values;
+
+    @Value("#{${dbValues}}")
+    private Map<String, String> dbValues;
 
     @GetMapping("/greeting")
     public String greeting() {
-        return myGreeting;
+        return myGreeting + values + dbValues;
     }
 }
