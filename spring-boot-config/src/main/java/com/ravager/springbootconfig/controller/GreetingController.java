afc91ac0d32aeaed04843b3529c1e0fbf6a4c53c
ahmsay <ahmsay@gmail.com>
2020-04-25 18:21:44 +0300

    spring cloud config client setup

--- spring-boot-config/src/main/java/com/ravager/springbootconfig/controller/GreetingController.java
+++ spring-boot-config/src/main/java/com/ravager/springbootconfig/controller/GreetingController.java
@@ -1,12 +1,16 @@
 package com.ravager.springbootconfig.controller;
 
 import com.ravager.springbootconfig.DbSettings;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@Profile("test")
 public class GreetingController {
+
+    @Value("${my.greeting}")
+    private String myGreeting;
 
     private DbSettings dbSettings;
 
@@ -16,6 +20,6 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting() {
-        return dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
+        return myGreeting + dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
     }
 }
